bbd26b4ccc90fbfb2c0eed06b2fd79312da84f62
Elmekkimahmoud <59558915+Elmekkimahmoud@users.noreply.github.com>
2020-04-20 10:39:46 +0100

    Updates

---
+++ precache-manifest.24852df0fa67515fdee54106f5c85b93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98062cae68d2c63fc603e1d15e513a75",
+    "url": "/Webtest/index.html"
+  },
+  {
+    "revision": "88ed2f7c1659fc2330ae",
+    "url": "/Webtest/static/css/main.cfcd7e28.chunk.css"
+  },
+  {
+    "revision": "622ad214b8f90412de0f",
+    "url": "/Webtest/static/js/2.cafaf2d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Webtest/static/js/2.cafaf2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ed2f7c1659fc2330ae",
+    "url": "/Webtest/static/js/main.2c7461c2.chunk.js"
+  },
+  {
+    "revision": "4dbbdbfb450353a8eb2d",
+    "url": "/Webtest/static/js/runtime-main.5ace34d0.js"
+  }
+]);
